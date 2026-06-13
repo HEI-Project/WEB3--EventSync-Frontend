447a88d66e31e9390381fb3b6bea7ce345b2c66f
Vals43 <hei.teddy.3@gmail.com>
2026-06-13 13:16:31 +0300

    fix: move admin inline in page.tsx without dynamic import

--- app/admin/[[...slug]]/admin-layout.tsx
+++ app/admin/[[...slug]]/admin-layout.tsx
@@ -1,12 +1,19 @@
-import { Layout } from 'react-admin';
-import { UserMenu, Logout } from 'react-admin';
+import { Layout, Menu, DashboardMenuItem } from 'react-admin';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import MicIcon from '@mui/icons-material/Mic';
+import PeopleIcon from '@mui/icons-material/People';
+import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
 
-const MyUserMenu = () => (
-  <UserMenu>
-    <Logout />
-  </UserMenu>
+const CustomMenu = () => (
+  <Menu>
+    <DashboardMenuItem to="/admin" />
+    <Menu.Item to="/admin/events" primaryText="Événements" leftIcon={<CalendarMonthIcon />} />
+    <Menu.Item to="/admin/sessions" primaryText="Sessions" leftIcon={<MicIcon />} />
+    <Menu.Item to="/admin/speakers" primaryText="Intervenants" leftIcon={<PeopleIcon />} />
+    <Menu.Item to="/admin/rooms" primaryText="Salles" leftIcon={<MeetingRoomIcon />} />
+  </Menu>
 );
 
 export const MyLayout = (props: any) => (
-  <Layout {...props} userMenu={<MyUserMenu />} />
+  <Layout {...props} menu={CustomMenu} />
 );
